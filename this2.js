80018efdc81c5c559342f1d3a4d7f5de04448af0
daniel <danma1696@gmail.com>
2024-07-21 20:49:38 +0200

    Tutorial 104

--- this2.js
+++ this2.js
@@ -1,4 +1,35 @@
+//VIDEO 104 This...Parte 4
+//let cadena = new String('Marcos');
+
+const objeto = {
+    titulo: 'Mis nombres',
+    nombres: ['Pedro', 'Raul', 'Maria'],
+    mostrarNombres() {
+        //con funcion flecha funciona, con funcion anonima no
+        this.nombres.forEach((nombre) => {
+            console.log(this.titulo,nombre);
+        })
+    }
+}
+objeto.mostrarNombres();
+
+//**Esta es una funcion constructora que crea un objeto */
+function Texto(nombre,apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    console.log(this);
+}
+
+//Esta no crea un objeto this hace referencia a window
+function otra() {
+    console.log(this);
+}
+otra();
+
+const nueva = new Texto('Hola mundo', 'Vivo en Zamora');
+
 const miObjeto = {
+    apellido: 'Castelli',
     dameNombre() {
         return 'Marcos';
     },
@@ -6,6 +37,7 @@ const miObjeto = {
         console.log(this);
         document.addEventListener('click', (evento) => {
             console.log(this.dameNombre());
+            console.log(this.apellido);
         })
     }
 }
